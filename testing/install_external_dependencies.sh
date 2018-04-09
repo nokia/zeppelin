361b0a95d2e8a9f2ee95d83f91b6b8ce0638112f
andrea <andrea.peruffo1982@gmail.com>
2018-04-09 14:28:43 +0100

    again

--- testing/install_external_dependencies.sh
+++ testing/install_external_dependencies.sh
@@ -90,7 +90,7 @@ if [[ -n "$PYTHON" ]] ; then
   cd ${HOME}
   virtualenv --python=$PYTHON_EXEC zeppelin
   source ${HOME}/zeppelin/bin/activate
-  pip install --upgrade setuptools grpcio bkzep==0.4.0 python-Levenshtein==0.12.0 cython==0.26.1 numpy==1.13.1 pandas==0.20.3 matplotlib==2.0.2 sympy==1.1.1 py4j==0.10.6 pattern==2.6 scipy==0.19.1 nltk==3.2.4 gensim==2.3.0 sklearn==0.0 stemming==1.0.1 ggplot==0.11.5 fuzzywuzzy==0.15.1 boto==2.48.0
+  pip install --upgrade setuptools grpcio bkzep==0.4.0 python-Levenshtein==0.12.0 cython==0.26.1 numpy==1.13.1 pandas==0.20.3 matplotlib==2.0.2 sympy==1.1.1 py4j==0.10.6 pattern==2.6 scipy==0.19.1 nltk==3.2.4 gensim==2.3.0 sklearn==0.0 stemming==1.0.1 ggplot==0.11.5 fuzzywuzzy==0.15.1
 else
   mkdir -p ${HOME}/zeppelin/bin
   touch ${HOME}/zeppelin/bin/activate
