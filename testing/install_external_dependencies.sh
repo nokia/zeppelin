834bce895c4b57525b0b6cebfc3901e6defd5c87
andrea <andrea.peruffo1982@gmail.com>
2018-04-09 10:38:59 +0100

    ..

--- testing/install_external_dependencies.sh
+++ testing/install_external_dependencies.sh
@@ -49,5 +49,5 @@ touch .environ
 # fi
 
 if [[ -n "$PYTHON" ]] ; then
-  pip install -q grpcio ggplot bkzep==0.4.0 python-Levenshtein==0.12.0 cython==0.26.1 numpy==1.13.1 pandas==0.20.3 matplotlib==2.0.2 sympy==1.1.1 py4j==0.10.6 pattern==2.6 scipy==0.19.1 nltk==3.2.4 gensim==2.3.0 sklearn==0.0 stemming==1.0.1 pyenchant==1.6.11 ggplot==0.11.5 fuzzywuzzy==0.15.1 boto==2.48.0
+  pip install -q grpcio bkzep==0.4.0 python-Levenshtein==0.12.0 cython==0.26.1 numpy==1.13.1 pandas==0.20.3 matplotlib==2.0.2 sympy==1.1.1 py4j==0.10.6 pattern==2.6 scipy==0.19.1 nltk==3.2.4 gensim==2.3.0 sklearn==0.0 stemming==1.0.1 pyenchant==1.6.11 ggplot==0.11.5 fuzzywuzzy==0.15.1 boto==2.48.0
 fi
